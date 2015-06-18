3a47527b40ba08bc0df0fe7269dcba61dadbe61e
Riccardo Pret <riky.pret@gmail.com>
2015-06-18 17:42:07 +0200

    Aggiornata la home del profilo utente

--- src/util/Configurazione.java
+++ src/util/Configurazione.java
@@ -3,11 +3,11 @@ package util;
 public class Configurazione {
 	
 	private Dispositivo dispositivo;
-	private Integer fPos;
-	private Integer fSms;
+	private Integer fPos;	//secondi
+	private Integer fSms;	//secondi
 	private boolean smsEnabled;
 	private boolean emailEnabled;
-	private Integer speedAlarm;
+	private Integer speedAlarm;		//km/h
 	
 	public Configurazione(Dispositivo dispositivo){
 		this.dispositivo=dispositivo;
@@ -56,6 +56,40 @@ public class Configurazione {
 	public void setspeedAlarm(Integer speed) {
 		this.speedAlarm = speed;
 	}
+	
+	//Customized
+	public String getFormattedFpos(){
+		int s, m, h;
+		h=this.fPos/3600;
+		m=(this.fPos%3600)/60;
+		s=(this.fPos%3600)%60;
+		return h+":"+m+":"+s;
+	}
+	
+	public void setFormattedFpos(String str){
+		int h=Integer.parseInt(str.split(":")[0]);
+		int m=Integer.parseInt(str.split(":")[1]);
+		int s=Integer.parseInt(str.split(":")[2]);
+		
+		this.fPos=s+m*60+h*3600;
+	}
+	
+	public String getFormattedFsms(){
+		int s, m, h;
+		h=this.fSms/3600;
+		m=(this.fSms%3600)/60;
+		s=(this.fSms%3600)%60;
+		return h+":"+m+":"+s;
+	}
+	
+	public void setFormattedFsms(String str){
+		int h=Integer.parseInt(str.split(":")[0]);
+		int m=Integer.parseInt(str.split(":")[1]);
+		int s=Integer.parseInt(str.split(":")[2]);
+		
+		this.fSms=s+m*60+h*3600;
+	}
+	
 /*
 	public void setAll(int fPos, int fSms, int speed, boolean smsEnabled,
 			boolean emailEnabled) {
